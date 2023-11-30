2ffed4dfa5d484ba9ca948b1d05c94afaf5523fa
manuelenrq9 <manuel.enrqt@gmail.com>
2023-11-30 19:15:11 -0400

    muchas mas funcionalidades

--- inscripcion.py
+++ inscripcion.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy import and_
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -59,7 +60,7 @@ class Calificacion(db.Model):
     cod_estudiante = db.Column(db.Integer, db.ForeignKey('estudiante.cod_estudiante'), primary_key=True)
     cod_asig = db.Column(db.Integer, db.ForeignKey('asignatura.cod_asig'), primary_key=True)
     nota = db.Column(db.Integer)
-    aprobado = aprobado = db.Column(db.Boolean)
+    aprobado = db.Column(db.Boolean)
     
     def __init__(self, cod_estudiante, cod_asig, nota):
         self.cod_estudiante = cod_estudiante
@@ -198,6 +199,36 @@ def cargar_nota(idEst,idAsig):
        
     else:
         return render_template('cargar_notas.html', estudiante=estudiante, asignatura=asignatura)
+    
+@app.route('/busqueda_regular', methods=['GET', 'POST','INSCRIBIR'])
+def buscar_regular():
+    estudiante = None
+    if request.method == 'POST':
+        cedula = request.form['CEDULA']
+        estudiante = Estudiante.query.filter_by(cedula=cedula).first_or_404(description="Estudiante no encontrado")
+        return render_template('busqueda_regular.html', estudiante=estudiante)        
+    else:
+        return render_template('busqueda_regular.html', estudiante = estudiante)
+    
+@app.route('/inscripcion_regular/<int:id>', methods=['GET', 'POST'])
+def inscribir_regular(id):
+    estudiante = Estudiante.query.get_or_404(id)
+    # Obtener las calificaciones aprobadas
+    aprobados = Calificacion.query.filter_by(aprobado=True).all()
+        
+    # Obtener los códigos de asignatura de las calificaciones aprobadas
+    codigos_aprobados = [calificacion.cod_asig for calificacion in aprobados]
+
+    # Obtener los códigos de asignatura cuyos requisitos estén aprobados
+    requisitos_aprobados = Requisito.query.filter(Requisito.cod_requisito.in_(codigos_aprobados)).all()
+    codigos_requisitos = [codigo.cod_asig for codigo in requisitos_aprobados]
+
+    # Obtener las asignaturas cuyos códigos estén en codigos_requisitos_aprobados y que no estén en codigos_aprobados
+    asignaturas = Asignatura.query.filter(and_(Asignatura.cod_asig.in_(codigos_requisitos), ~Asignatura.cod_asig.in_(codigos_aprobados))).all()
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('inscripcion_regular.html', estudiante=estudiante, asignaturas=asignaturas)
     
 
 @app.route('/delete/<int:id>')
@@ -345,13 +376,9 @@ def asignaturas_enfermeria():
     insert_asignatura(pasantia)
     teg = Asignatura(cod_asig=100268,nombre="Trabajo Especial de Grado",unidad_creditos=0)
     insert_asignatura(teg)
- 
- #################################   
-class Requisito(db.Model):
-    cod_requisito = db.Column(db.Integer, db.ForeignKey('asignatura.cod_asig'), primary_key=True)
-    cod_asig = db.Column(db.Integer, db.ForeignKey('asignatura.cod_asig'), primary_key=True)
- ##################################   
     
+    
+       
 def requisitos_enfermeria():
     #Segundo Semestre
     micro_1 = Requisito(cod_requisito=1150,cod_asig=2152)
@@ -378,30 +405,36 @@ def requisitos_enfermeria():
     #Tercer Semestre
     fisio = Requisito(cod_requisito=2153,cod_asig=3154)
     insert_requisito(fisio)
-    farmaco_1 = Requisito(cod_requisito=1150,cod_asig=3155)
-    insert_requisito(farmaco_1)
-    farmaco_2 = Requisito(cod_requisito=1151,cod_asig=3155)
-    insert_requisito(farmaco_2)
+    #farmaco_1 = Requisito(cod_requisito=1150,cod_asig=3155)
+    #insert_requisito(farmaco_1)
+    #farmaco_2 = Requisito(cod_requisito=1151,cod_asig=3155)
+    #insert_requisito(farmaco_2)
+    farmaco_3 = Requisito(cod_requisito=2153,cod_asig=3155)
+    insert_requisito(farmaco_3)
     EnfMedica= Requisito(cod_requisito=2256,cod_asig=3257)
     insert_requisito(EnfMedica)
     bioEpi= Requisito(cod_requisito=2370,cod_asig=3373)
     insert_requisito(bioEpi)
-    inglesI= Requisito(cod_requisito=1479,cod_asig=3481)
-    insert_requisito(inglesI)
+    #inglesI_1= Requisito(cod_requisito=1479,cod_asig=3481)
+    #insert_requisito(inglesI_1)
+    inglesI_2= Requisito(cod_requisito=2370,cod_asig=3481)
+    insert_requisito(inglesI_2)
     
     #Cuarto Semestre
     mentalyPsiq= Requisito(cod_requisito=3257,cod_asig=4258)
     insert_requisito(mentalyPsiq)
-    metodInv= Requisito(cod_requisito=2370,cod_asig=4371)
-    insert_requisito(metodInv)
+    #metodInv= Requisito(cod_requisito=2370,cod_asig=4371)
+    #insert_requisito(metodInv)
+    metodInv_1= Requisito(cod_requisito=3257,cod_asig=4371)
+    insert_requisito(metodInv_1)
     maternoI= Requisito(cod_requisito=3257,cod_asig=4372)
     insert_requisito(maternoI)
     inglesII= Requisito(cod_requisito=3481,cod_asig=4482)
     insert_requisito(inglesII)
     
     #Quinto Semestre
-    enfQuirur_1= Requisito(cod_requisito=3257,cod_asig=5259)
-    insert_requisito(enfQuirur_1)
+    #enfQuirur_1= Requisito(cod_requisito=3257,cod_asig=5259)
+    #insert_requisito(enfQuirur_1)
     enfQuirur_2= Requisito(cod_requisito=4258,cod_asig=5259)
     insert_requisito(enfQuirur_2)
     enfQuirur_3= Requisito(cod_requisito=4372,cod_asig=5259)
@@ -410,34 +443,34 @@ def requisitos_enfermeria():
     insert_requisito(maternoII_1)
     maternoII_2= Requisito(cod_requisito=4372,cod_asig=5374)
     insert_requisito(maternoII_2)
-    adminEnf_1= Requisito(cod_requisito=3257,cod_asig=5375)
-    insert_requisito(adminEnf_1)
+    #adminEnf_1= Requisito(cod_requisito=3257,cod_asig=5375)
+    #insert_requisito(adminEnf_1)
     adminEnf_2= Requisito(cod_requisito=4258,cod_asig=5375)
     insert_requisito(adminEnf_2)
     adminEnf_3= Requisito(cod_requisito=4372,cod_asig=5375)
     insert_requisito(adminEnf_3)
     
     #Sexto Semestre
-    interRot_1= Requisito(cod_requisito=3373,cod_asig=6260)
-    insert_requisito(interRot_1)
-    interRot_2= Requisito(cod_requisito=4258,cod_asig=6260)
-    insert_requisito(interRot_2)
-    interRot_3= Requisito(cod_requisito=4371,cod_asig=6260)
-    insert_requisito(interRot_3)
-    interRot_4= Requisito(cod_requisito=4372,cod_asig=6260)
-    insert_requisito(interRot_4)
+    #interRot_1= Requisito(cod_requisito=3373,cod_asig=6260)
+    #insert_requisito(interRot_1)
+    #interRot_2= Requisito(cod_requisito=4258,cod_asig=6260)
+    #insert_requisito(interRot_2)
+    #interRot_3= Requisito(cod_requisito=4371,cod_asig=6260)
+    #insert_requisito(interRot_3)
+    #interRot_4= Requisito(cod_requisito=4372,cod_asig=6260)
+    #insert_requisito(interRot_4)
     interRot_5= Requisito(cod_requisito=5259,cod_asig=6260)
     insert_requisito(interRot_5)
     interRot_6= Requisito(cod_requisito=5374,cod_asig=6260)
     insert_requisito(interRot_6)
-    servCom_1= Requisito(cod_requisito=3373,cod_asig=6261)
-    insert_requisito(servCom_1)
-    servCom_2= Requisito(cod_requisito=4258,cod_asig=6261)
-    insert_requisito(servCom_2)
-    servCom_3= Requisito(cod_requisito=4371,cod_asig=6261)
-    insert_requisito(servCom_3)
-    servCom_4= Requisito(cod_requisito=4372,cod_asig=6261)
-    insert_requisito(servCom_4)
+    #servCom_1= Requisito(cod_requisito=3373,cod_asig=6261)
+    #insert_requisito(servCom_1)
+    #servCom_2= Requisito(cod_requisito=4258,cod_asig=6261)
+    #insert_requisito(servCom_2)
+    #servCom_3= Requisito(cod_requisito=4371,cod_asig=6261)
+    #insert_requisito(servCom_3)
+    #servCom_4= Requisito(cod_requisito=4372,cod_asig=6261)
+    #insert_requisito(servCom_4)
     servCom_5= Requisito(cod_requisito=5259,cod_asig=6261)
     insert_requisito(servCom_5)
     servCom_6= Requisito(cod_requisito=5374,cod_asig=6261)
@@ -476,18 +509,19 @@ def requisitos_enfermeria():
     insert_requisito(adminServ)
     
     #Decimo semestre
-    pasantias_1= Requisito(cod_requisito=8263,cod_asig=100267)
-    insert_requisito(pasantias_1)
-    pasantias_2= Requisito(cod_requisito=8265,cod_asig=100267)
-    insert_requisito(pasantias_2)
+    #pasantias_1= Requisito(cod_requisito=8263,cod_asig=100267)
+    #insert_requisito(pasantias_1)
+    #pasantias_2= Requisito(cod_requisito=8265,cod_asig=100267)
+    #insert_requisito(pasantias_2)
     pasantias_3= Requisito(cod_requisito=9265,cod_asig=100267)
     insert_requisito(pasantias_3)
-    teg_1 = Requisito(cod_requisito=8265,cod_asig=100268)
-    insert_requisito(teg_1)
+    #teg_1 = Requisito(cod_requisito=8265,cod_asig=100268)
+    #insert_requisito(teg_1)
     teg_2 = Requisito(cod_requisito=9265,cod_asig=100268)
     insert_requisito(teg_2)
     
 if __name__ == "__main__":
     asignaturas_enfermeria()
+    requisitos_enfermeria()
     app.run(debug=True)
     
